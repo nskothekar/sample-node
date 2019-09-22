bfe89e4ad681e186268336ec66d12a3bd94e125e
nitish <nitishk@valueaddsofttech.com>
2019-09-22 11:56:06 +0530

    update branch name

--- code/server.js
+++ code/server.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-<<<<<<< HEAD:code/server.js
-  res.send('This is Dev!')
-=======
-  res.send('This is Dev Branch!')
->>>>>>> d05310af263ebbfdafe61440497b7af660b22435:code/server.js
+  res.send('This is  Master Branch!')
 })
 
 app.listen(3000, function () {
